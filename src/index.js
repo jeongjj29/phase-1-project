cff703e5bba6a8cc95ed10d4d8d3f3501288e988
Jae Jeong <jeongjj29@gmail.com>
2024-07-30 17:34:34 -0400

    Add css styling and close button for description box

--- src/index.js
+++ src/index.js
@@ -39,6 +39,11 @@ function displayCards(searchValue) {
     });
 }
 
+function handleCloseButton() {
+  descriptionDiv.innerHTML = "";
+  descriptionDiv.className = "hidden";
+}
+
 // creates cards and appends them to the collectionDiv
 function createChampionCard(champId, champName) {
   const cardDiv = document.createElement("div");
@@ -88,14 +93,17 @@ async function convertImageToBase64(url) {
 const searchBar = document.querySelector("#search");
 searchBar.addEventListener("submit", (e) => {
   e.preventDefault();
+  handleCloseButton();
   searchValue = searchBar.query.value.toLowerCase();
   displayCards(searchValue);
+  this.query.value = "";
 });
 
 const checkboxes = document.querySelectorAll("input[type='checkbox']");
 const selectedClasses = new Set();
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener("change", (event) => {
+    handleCloseButton();
     const checkbox = event.target;
     if (checkbox.checked) {
       selectedClasses.add(checkbox.id);
@@ -136,18 +144,18 @@ function handleChampionClick(championName) {
       const info = championData.info;
 
       // IMAGE DIV
-      const imageDiv = document.createElement("div");
-      imageDiv.id = "splashArt";
+      const imageSection = document.createElement("section");
+      imageSection.id = "splashArt";
       const splashArtImg = document.createElement("img");
       const champImgUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championName}_0.jpg`;
       convertImageToBase64(champImgUrl).then((dataUrl) => {
         splashArtImg.src = dataUrl;
       });
-      imageDiv.append(splashArtImg);
+      imageSection.append(splashArtImg);
 
       // NAME/TITLE DIV
-      const nameTitleDiv = document.createElement("div");
-      nameTitleDiv.id = "name-title";
+      const nameTitleSection = document.createElement("section");
+      nameTitleSection.id = "name-title";
       const nameH2 = document.createElement("h2");
       nameH2.textContent = name + " ";
       nameH2.class = "championName";
@@ -155,24 +163,33 @@ function handleChampionClick(championName) {
       const titleH3 = document.createElement("h3");
       titleH3.textContent = title;
       titleH3.className = "championTitle";
-      nameTitleDiv.append(nameH2, titleH3);
+
+      const closeDescriptionButton = document.createElement("button");
+      closeDescriptionButton.textContent = "X";
+      closeDescriptionButton.id = "close-description-box";
+      closeDescriptionButton.addEventListener("click", () => {
+        handleCloseButton();
+      });
+
+      nameTitleSection.append(nameH2, titleH3);
 
       // LORE DIV
-      const loreDiv = document.createElement("div");
-      loreDiv.id = "lore";
+      const loreSection = document.createElement("section");
+      loreSection.id = "lore";
       const loreH2 = document.createElement("h2");
-      loreH2.textContent = "Lore:";
+      loreH2.textContent = "Lore";
       const loreP = document.createElement("p");
       loreP.textContent = lore;
-      loreDiv.append(loreH2, loreP);
+      loreSection.append(loreH2, loreP);
 
       // DATA DIV
-      const dataDiv = document.createElement("div");
-      dataDiv.id = "data";
+      const dataSection = document.createElement("section");
+      dataSection.id = "data";
 
       const classDiv = document.createElement("div");
+      classDiv.className = "tag";
       const classH3 = document.createElement("h2");
-      classH3.textContent = "Classes:";
+      classH3.textContent = "Classes";
       classDiv.append(classH3);
       tags.forEach(function (tag) {
         const tagP = document.createElement("p");
@@ -181,131 +198,181 @@ function handleChampionClick(championName) {
       });
 
       const resourceDiv = document.createElement("div");
+      resourceDiv.className = "tag";
       const resourceH3 = document.createElement("h2");
       const resourceP = document.createElement("p");
-      resourceH3.textContent = "Resource Type:";
+      resourceH3.textContent = "Resource Type";
       resourceP.textContent = partype;
       resourceDiv.append(resourceH3, resourceP);
 
       const ratingDiv = document.createElement("div");
+      ratingDiv.className = "tag";
       const ratingH3 = document.createElement("h2");
-      ratingH3.textContent = "Ratings:";
+      ratingH3.textContent = "Ratings";
       ratingDiv.append(ratingH3);
       for (let rating in info) {
         const ratingP = document.createElement("p");
         const ratingSpan = document.createElement("span");
+        ratingSpan.className = "rating-title";
         const formattedRatingText =
           rating.charAt(0).toUpperCase() + rating.slice(1);
         ratingSpan.textContent = formattedRatingText + ": ";
         const ratingValueSpan = document.createElement("span");
+        ratingValueSpan.className = "rating-value";
         ratingValueSpan.textContent = info[rating];
         ratingP.append(ratingSpan, ratingValueSpan);
         ratingDiv.append(ratingP);
       }
 
-      dataDiv.append(classDiv, resourceDiv, ratingDiv);
+      dataSection.append(classDiv, resourceDiv, ratingDiv);
 
       // STATS
-      const statsDiv = document.createElement("div");
-      statsDiv.id = "stats";
+      const statsSection = document.createElement("section");
+      statsSection.id = "stats";
       const statsH2 = document.createElement("h2");
-      statsH2.textContent = "Champion Stats:";
+      statsH2.textContent = "Champion Stats";
 
-      const hpP = document.createElement("p");
-      hpP.textContent = "HP: ";
+      const hpDiv = document.createElement("div");
+      hpDiv.className = "stat";
+      const hpH4 = document.createElement("h4");
+      hpH4.textContent = "Health";
       const hpValueSpan = document.createElement("span");
       hpValueSpan.textContent = stats.hp;
       const hpPerLvlSpan = document.createElement("span");
       hpPerLvlSpan.textContent = ` ( +${stats.hpperlevel} per Level )`;
-      hpP.append(hpValueSpan, hpPerLvlSpan);
+      const hpSpan = document.createElement("span");
+      hpDiv.append(hpH4, hpValueSpan, hpPerLvlSpan);
 
-      const mpP = document.createElement("p");
-      mpP.textContent = "MP: ";
+      const mpDiv = document.createElement("div");
+      mpDiv.className = "stat";
+      const mpH4 = document.createElement("h4");
+      mpH4.textContent = "Resource";
       const mpValueSpan = document.createElement("span");
-      mpValueSpan.textContent = stats.mp;
+      mpValueSpan.textContent = stats.mp ? stats.mp : "N/A";
       const mpPerLvlSpan = document.createElement("span");
-      mpPerLvlSpan.textContent = ` ( +${stats.mpperlevel} per Level )`;
-      mpP.append(mpValueSpan, mpPerLvlSpan);
+      mpPerLvlSpan.textContent = stats.mpperlevel
+        ? ` ( +${stats.mpperlevel} per Level )`
+        : "";
+      if (stats.mp) {
+        mpDiv.append(mpH4, mpValueSpan, mpPerLvlSpan);
+      } else {
+        mpDiv.append(mpH4, mpValueSpan);
+      }
 
-      const hpregenP = document.createElement("p");
-      hpregenP.textContent = "HP Regen: ";
+      const hpregenDiv = document.createElement("div");
+      hpregenDiv.className = "stat";
+      const hpregenH4 = document.createElement("h4");
+      hpregenH4.textContent = "Health Regen";
       const hpregenValueSpan = document.createElement("span");
       hpregenValueSpan.textContent = stats.hpregen;
       const hpregenPerLvlSpan = document.createElement("span");
       hpregenPerLvlSpan.textContent = ` ( +${stats.hpregenperlevel} per Level )`;
-      hpregenP.append(hpregenValueSpan, hpregenPerLvlSpan);
+      hpregenDiv.append(hpregenH4, hpregenValueSpan, hpregenPerLvlSpan);
 
-      const mpregenP = document.createElement("p");
-      mpregenP.textContent = "MP Regen: ";
+      const mpregenDiv = document.createElement("div");
+      mpregenDiv.className = "stat";
+      const mpregenH4 = document.createElement("h4");
+      mpregenH4.textContent = "Resource Regen";
       const mpregenValueSpan = document.createElement("span");
-      mpregenValueSpan.textContent = stats.mpregen;
+      mpregenValueSpan.textContent = stats.mpregen ? stats.mpregen : "N/A";
       const mpregenPerLvlSpan = document.createElement("span");
-      mpregenPerLvlSpan.textContent = ` ( +${stats.mpregenperlevel} per Level )`;
-      mpregenP.append(mpregenValueSpan, mpregenPerLvlSpan);
+      mpregenPerLvlSpan.textContent = stats.mpregenperlevel
+        ? ` ( +${stats.mpregenperlevel} per Level )`
+        : "";
+      if (stats.mp) {
+        mpregenDiv.append(mpregenH4, mpregenValueSpan, mpregenPerLvlSpan);
+      } else {
+        mpregenDiv.append(mpregenH4, mpregenValueSpan);
+      }
 
-      const armorP = document.createElement("p");
-      armorP.textContent = "Armor: ";
+      const armorDiv = document.createElement("div");
+      armorDiv.className = "stat";
+      const armorH4 = document.createElement("h4");
+      armorH4.textContent = "Armor";
       const armorValueSpan = document.createElement("span");
       armorValueSpan.textContent = stats.armor;
       const armorPerLvlSpan = document.createElement("span");
       armorPerLvlSpan.textContent = ` ( +${stats.armorperlevel} per Level )`;
-      armorP.append(armorValueSpan, armorPerLvlSpan);
+      armorDiv.append(armorH4, armorValueSpan, armorPerLvlSpan);
 
-      const mrP = document.createElement("p");
-      mrP.textContent = "Magic Resist: ";
+      const mrDiv = document.createElement("div");
+      mrDiv.className = "stat";
+      const mrH4 = document.createElement("h4");
+      mrH4.textContent = "Magic Resist";
       const mrValueSpan = document.createElement("span");
       mrValueSpan.textContent = stats.spellblock;
       const mrPerLvlSpan = document.createElement("span");
       mrPerLvlSpan.textContent = ` ( +${stats.spellblockperlevel} per Level )`;
-      mrP.append(mrValueSpan, mrPerLvlSpan);
+      mrDiv.append(mrH4, mrValueSpan, mrPerLvlSpan);
 
-      const adP = document.createElement("p");
-      adP.textContent = "Attack Damage: ";
+      const adDiv = document.createElement("div");
+      adDiv.className = "stat";
+      const adH4 = document.createElement("h4");
+      adH4.textContent = "Attack Damage";
       const adValueSpan = document.createElement("span");
       adValueSpan.textContent = stats.attackdamage;
       const adPerLvlSpan = document.createElement("span");
       adPerLvlSpan.textContent = ` ( +${stats.attackdamageperlevel} per Level )`;
-      adP.append(adValueSpan, adPerLvlSpan);
+      adDiv.append(adH4, adValueSpan, adPerLvlSpan);
 
-      const asP = document.createElement("p");
-      asP.textContent = "Attack Speed: ";
+      const asDiv = document.createElement("div");
+      asDiv.className = "stat";
+      const asH4 = document.createElement("h4");
+      asH4.textContent = "Attack Speed: ";
       const asValueSpan = document.createElement("span");
       asValueSpan.textContent = stats.attackspeed;
       const asPerLevelSpan = document.createElement("span");
-      asPerLevelSpan.textContent = ` ( +${stats.attackspeedperlevel} per Level )`;
-      asP.append(asValueSpan, asPerLevelSpan);
+      asPerLevelSpan.textContent = ` ( +${stats.attackspeedperlevel}% per Level )`;
+      asDiv.append(asH4, asValueSpan, asPerLevelSpan);
 
-      const movespdP = document.createElement("p");
-      movespdP.textContent = "Movement Speed: ";
+      const movespdDiv = document.createElement("div");
+      movespdDiv.className = "stat";
+      const movespdH4 = document.createElement("h4");
+      movespdH4.textContent = "Movement Speed: ";
       const movespdValueSpan = document.createElement("span");
       movespdValueSpan.textContent = stats.movespeed;
-      movespdP.append(movespdValueSpan);
+      movespdDiv.append(movespdH4, movespdValueSpan);
 
-      const atkrngP = document.createElement("p");
-      atkrngP.textContent = "Attack Range: ";
+      const atkrngDiv = document.createElement("div");
+      atkrngDiv.className = "stat";
+      const atkrngH4 = document.createElement("h4");
+      atkrngH4.textContent = "Attack Range: ";
       const atkrngValueSpan = document.createElement("span");
       atkrngValueSpan.textContent = stats.attackrange;
-      atkrngP.append(atkrngValueSpan);
+      atkrngDiv.append(atkrngH4, atkrngValueSpan);
 
-      statsDiv.append(
+      const statsContainerDiv = document.createElement("div");
+      statsContainerDiv.id = "stats-container";
+      statsContainerDiv.append(
         statsH2,
-        hpP,
-        mpP,
-        hpregenP,
-        mpregenP,
-        armorP,
-        mrP,
-        adP,
-        asP,
-        movespdP,
-        atkrngP
+        hpDiv,
+        hpregenDiv,
+        mpDiv,
+        mpregenDiv,
+        armorDiv,
+        mrDiv,
+        adDiv,
+        asDiv,
+        movespdDiv,
+        atkrngDiv
       );
+      statsSection.append(statsContainerDiv);
 
       const infoContainerDiv = document.createElement("div");
       infoContainerDiv.id = "champion-info";
-      infoContainerDiv.append(imageDiv, loreDiv, statsDiv, dataDiv);
+      infoContainerDiv.append(
+        imageSection,
+        loreSection,
+        statsSection,
+        dataSection
+      );
 
-      descriptionDiv.append(nameTitleDiv, infoContainerDiv);
+      descriptionDiv.append(
+        nameTitleSection,
+        infoContainerDiv,
+        closeDescriptionButton
+      );
+      descriptionDiv.className = "";
     });
 }
 
